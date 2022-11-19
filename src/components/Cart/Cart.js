d70c099e9b5396652f8cb73c220dea7ad3e84edd
DIPANKAR <you@example.com>
2022-11-19 09:59:26 +0600

    data add to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,14 +3,24 @@ import './Cart.css';
 
 const Cart = (props) => {
     const cart = props.cart;
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        console.log(product);
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+
+    }
+    const tax = parseFloat((total * .10).toFixed(2));
+    const grandTotal = (total + shipping + tax).toFixed(2);
     return (
         <div className="cart">
             <h2>Order Summary:</h2>
             <p>Selected Items: {cart.length}</p>
-            <p><strong>Total Price: $</strong></p>
-            <p><strong>Shipping Charge: $ </strong> </p>
-            <p><strong>Tax: $ </strong></p>
-            <h4>Grand Total: $ </h4>
+            <p><strong>Total Price: $ {total}</strong></p>
+            <p><strong>Shipping Charge: $ {shipping} </strong> </p>
+            <p><strong>Tax: $ {tax} </strong></p>
+            <h4>Grand Total: $ {grandTotal} </h4>
         </div>
     );
 };
